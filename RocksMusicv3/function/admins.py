e76aa55fd20dcc6c79fe5b96505b88914d312ce5
Abhimnayu Singh Ranawat <92500846+AbhimanyuSinghRana@users.noreply.github.com>
2021-10-21 07:10:20 +0530

    Update admins.py

--- RocksMusicv3/function/admins.py
+++ RocksMusicv3/function/admins.py
@@ -1,7 +1,7 @@
 # A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
 # Without Credit (Mother Fucker)
-# Rocks © @Dr_Asad_Ali © Rocks
-# Owner Asad + Harshit
+# @Venom_Hai_Hum 
+# Owner AbhimanyuSinghRana 
 
 from typing import Dict
 from typing import List
